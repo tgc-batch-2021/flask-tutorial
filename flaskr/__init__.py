057182805ab873786fab6539f5af16a7dc0fbf0c
Jay Modi <jay.modi@thegatewaycorp.com>
2021-01-25 11:35:43 +0530

    Initilize SQLite DB

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -16,12 +16,14 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(text_config)
 
-
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    from . import db
+    db.init_app(app)
 
     # a simple page that says hello
     @app.route('/hello')
